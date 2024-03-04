89c6cf8d1cf91d8be20689a3d355df8dcbb5116e
raisaafadilla <raisa.fadilla@ui.ac.id>
2024-03-04 22:52:57 +0700

    [REFACTOR] Add OrderStatus enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -2,9 +2,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
